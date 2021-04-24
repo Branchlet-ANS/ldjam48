eb4b2304bd0a1aaa16b6b91f958fe50a3bb18a3c
magnetenstad <tenstad.magne@gmail.com>
2021-04-24 20:36:52 +0200

    Cleanup

--- godot/Character.gd
+++ godot/Character.gd
@@ -5,23 +5,14 @@ class_name Character
 onready var scene_item = preload("res://Item.gd")
 onready var collision_shape = $"CollisionShape2D"
 
-var inventory : Inventory
+var inventory : Inventory = Inventory.new()
 var state : int
 var velocity : Vector2 = Vector2(0, 0)
 var target : Vector2 = Vector2(0, 0)
 var speed : float = 100
 
 func _init():
-	inventory = Inventory.new()
-	inventory.add(Food.new("Apple"))
-	inventory.add(Food.new("Apple"))
-	inventory.add(Food.new("Apple"))
-	inventory.add(Food.new("Banana"))
-	inventory.add(Food.new("Banana"))
-	
-	inventory.remove("Apple", 2)
-	
-	print(inventory._to_string())
+	pass
 	
 func _physics_process(delta):
 	velocity = transform.origin.direction_to(target) * speed
@@ -30,7 +21,7 @@ func _physics_process(delta):
 		target = transform.origin
 
 func _on_Area2D_body_entered(body):
-	if body.get_filename() == "res://Item.tscn":
+	if body.get_filename() == "res://Gatherable.tscn":
 		inventory.add(body.item)
 		body.queue_free()
 	
