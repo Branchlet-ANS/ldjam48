6ce568bcab3a51836dba8a2be21fe11af8a32e80
magnetenstad <tenstad.magne@gmail.com>
2021-04-24 17:21:58 +0200

    Add position parameter to add_character()

--- godot/Main.gd
+++ godot/Main.gd
@@ -22,7 +22,9 @@ func _ready():
 	var item = scene_item.instance()
 	item.transform.origin = Vector2(100, 100)
 	add_child(item)
-	add_character()
+	add_character(0, 0)
+	add_character(-100, -100)
+	
 #
 #	for i in range(-MAP_WIDTH/2, MAP_WIDTH/2):
 #		for j in range(-MAP_WIDTH/2, MAP_WIDTH/2):
@@ -42,8 +44,9 @@ func _ready():
 #				ill.set_color(Color.green)
 
 
-func add_character() -> Character:
+func add_character(x : int, y : int) -> Character:
 	var character = scene_character.instance()
+	character.transform.origin = Vector2(x, y)
 	characters.append(character)
 	add_child(character)
 	god.selected_characters.append(character) # temp
