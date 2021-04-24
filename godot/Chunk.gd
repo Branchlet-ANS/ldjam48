280cd85ca5f5dd997a218158c46fc50f673d8e06
Hans-Marius Øverås <hansmariusoveras@gmail.com>
2021-04-24 15:50:22 +0100

    chunk generation

--- godot/Chunk.gd
+++ godot/Chunk.gd
@@ -1,32 +1,22 @@
 
 class_name Chunk
-var _coordinates: Vector2
-var CORRUPTION_MAX = 25
-var _type: int
-var _left: Chunk
-var _right: Chunk
-var _up: Chunk
-var _down: Chunk
-var _corruption: int
+enum Type {BUSH, PATH, OPEN}
 
-func _init(coordinates, type: int, left: Chunk, right: Chunk, up: Chunk, down: Chunk, corruption: int):
+var _coordinates: Vector2
+var _type: int
+var _corruption: float
+
+func _init(coordinates: Vector2, type: int, corruption: float):
 	_coordinates = coordinates
 	_type = type
-	
-	_left = left
-	_right = right
-	_up = up
-	_down = down
-	
-	if _left == null and corruption < CORRUPTION_MAX:
-		_left = Chunk.new(_coordinates+Vector2.LEFT, chooseType(Vector2.LEFT), null, self, null, null, corruption+1)
-	if _right == null and corruption < CORRUPTION_MAX:
-		_right = Chunk.new(_coordinates+Vector2.RIGHT, chooseType(Vector2.RIGHT), self, null, null, null, corruption+1)
-	if _up == null and corruption < CORRUPTION_MAX:
-		_up = Chunk.new(_coordinates+Vector2.UP, chooseType(Vector2.UP), null, null, null, self, corruption+1)
-	if _down == null and corruption < CORRUPTION_MAX:
-		_down = Chunk.new(_coordinates+Vector2.DOWN, chooseType(Vector2.DOWN), null, null, self, null, corruption+1)
+	_corruption = corruption
 
-func chooseType(direction: Vector2):
-	#new_coordinates = _coordinates + direction
-	return 0
+func _to_string():
+	if _type == Type.BUSH:
+		return "BUSH"
+	elif _type == Type.OPEN:
+		return "OPEN"
+	elif _type == Type.PATH:
+		return "PATH"
+	else:
+		return "NULL"
--- godot/Main.gd
+++ godot/Main.gd
@@ -3,14 +3,37 @@ extends Node
 class_name Main
 
 onready var scene_character = preload("res://Character.tscn")
+onready var scene_chunk = preload("res://ChunkIllustration.tscn")
 onready var god : God = $God
 
+
+enum Type {BUSH, PATH, OPEN}
 const WIDTH = 640
 const HEIGHT = 360
+const MAP_WIDTH = 32
+const MAX_BRANCHES = 3
+
+var map = {}
+
 
 var characters : Array = []
 
 func _ready():
+	for i in range(-MAP_WIDTH/2, MAP_WIDTH/2):
+		for j in range(-MAP_WIDTH/2, MAP_WIDTH/2):
+			var pos = Vector2(i, j)
+			map[pos] = Chunk.new(pos, Type.BUSH, pos.length())
+	make_paths()
+	
+	for i in range(-MAP_WIDTH/2, MAP_WIDTH/2):
+		for j in range(-MAP_WIDTH/2, MAP_WIDTH/2):
+			var pos = Vector2(i, j)
+			var type = map[pos]._type
+			var ill = scene_chunk.instance()
+			add_child(ill)
+			ill.position = map[pos]._coordinates * 16
+			if type == 0:
+				ill.set_color(Color.green)
 	add_character()
 	
 func add_character() -> Character:
@@ -19,3 +42,25 @@ func add_character() -> Character:
 	add_child(character)
 	god.selected_characters.append(character)
 	return character
+
+func make_paths():
+	var directions = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+	var direction
+	randomize()
+	var scores = [0]
+	var current_position = Vector2(0, 0)
+	map[current_position] = Chunk.new(current_position, Type.OPEN, current_position.length())
+	for i in range(MAX_BRANCHES):
+		scores = [0]
+		current_position = Vector2(0, 0)
+		while scores.min() < MAP_WIDTH:
+			if scores.size() > 5:
+				scores.erase(scores.min())
+			randomize()
+			direction = directions[rand_range(0, 4)]
+			while (current_position+direction).length() < scores.min():
+				randomize()
+				direction = directions[rand_range(0, 4)]
+			current_position += direction
+			scores.append(current_position.length())
+			map[current_position] = Chunk.new(current_position, Type.OPEN, current_position.length())
