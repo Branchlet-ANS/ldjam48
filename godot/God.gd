2a63f22e8bfa2f96095263951b491a139b2657c7
magnetenstad <tenstad.magne@gmail.com>
2021-04-24 21:50:34 +0200

    Edit Character states and interactables

--- godot/God.gd
+++ godot/God.gd
@@ -10,7 +10,7 @@ var select_pressed = false
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
-		if(event.get_button_index() == 1):
+		if(event.get_button_index() == 2):
 			if(event.is_pressed()):
 				select_pos_start = camera.screen_position(event)
 				select_pressed = true
@@ -26,19 +26,19 @@ func _unhandled_input(event):
 							max(select_pos_start.y, select_pos_end.y) - min(select_pos_start.y, select_pos_end.y)).intersects(
 								Rect2(character.position, character.collision_shape.shape.get_extents()))):
 						selected_characters.append(character)
-				for interactable in get_parent().interactables:
-					if (interactable.transform.origin - event.position).length() < 8:
-						interact(interactable)
-						break
-		
-		elif(event.get_button_index() == 2):
+		elif(event.get_button_index() == 1):
+			for interactable in get_parent().get_interactables():
+				if (interactable.transform.origin - camera.screen_position(event)).length() < 16:
+					interact(interactable)
+					return
 			for character in selected_characters:
 				character.target = camera.screen_position(event)
-
+				character.set_state(character.STATE.target)
+				
 func interact(interactable):
 	for character in get_parent().characters:
 		character.add_job(interactable)
-
+		
 func _process(delta):
 	if select_pressed or selected_characters.size() > 0:
 		update()
--- godot/Main.gd
+++ godot/Main.gd
@@ -4,6 +4,8 @@ class_name Main
 
 onready var scene_gatherable = preload("res://Gatherable.tscn")
 onready var scene_character = preload("res://Character.tscn")
+onready var scene_interactable = preload("res://InteractableTest.tscn")
+
 onready var god : God = $God
 
 var characters : Array = []
@@ -15,7 +17,9 @@ func _ready():
 #	add_child(item)
 	add_character(0, 0)
 	add_character(-100, -100)
-
+	var interactable = scene_interactable.instance()
+	add_child(interactable)
+	interactables.append(interactable)
 
 func add_character(x : int, y : int) -> Character:
 	var character = scene_character.instance()
@@ -23,3 +27,12 @@ func add_character(x : int, y : int) -> Character:
 	characters.append(character)
 	add_child(character)
 	return character
+
+func get_interactables():
+	var i = 0
+	while i < interactables.size():
+		if (!is_instance_valid(interactables[i])):
+			interactables.erase(interactables[i])
+			i -= 1
+		i += 1
+	return interactables
