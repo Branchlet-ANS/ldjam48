222789d7dd121fcd9c4718a989a68b36973ae4fa
Erikseeeeen <43137654+Erikseeeeen@users.noreply.github.com>
2021-04-24 18:47:19 +0200

    God uses character rect instead of pos

--- godot/Character.gd
+++ godot/Character.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 class_name Character
 
 onready var scene_item = preload("res://Item.gd")
+onready var collision_shape = $"Area2D/CollisionShape2D"
 
 var inventory : Inventory
 var state : int
