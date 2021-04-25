167fba93bbc8d5c202a5acfa2551e0c18b168e0c
magnetenstad <tenstad.magne@gmail.com>
2021-04-25 22:26:25 +0200

    Clamp camera

--- godot/Main.gd
+++ godot/Main.gd
@@ -30,7 +30,7 @@ func _ready():
 	roomManager.set_tileset(load("res://world/tileset.tres"))
 	var _room
 	for i in range(32):
-		_room = Room.new(randi() % 32 + 16, randi() % 46 + 16, i)
+		_room = Room.new(randi() % 32 + 32, randi() % 46 + 32, i)
 		var r = randi() % 3
 		if r == 0:
 			_room.monster_room()
@@ -45,9 +45,6 @@ func _ready():
 		add_character(0, 0)
 	roomManager.rebuild()
 	
-	
-
-
 	god = God.new()
 	add_child(god)
 
@@ -59,18 +56,23 @@ func add_character(x : int, y : int):
 	characters.append(character)
 
 func _process(_delta):
+	var target = Vector2.ZERO
 	if god.selected_characters.size() > 0:
-		var target = Vector2.ZERO
 		var max_dis = 0
 		for character in god.selected_characters:
 			target += character.get_position()
 			for other in characters:
 				max_dis = max(max_dis, (other.get_position() - character.get_position()).length())
 		target /= god.selected_characters.size()
-		camera.target = target
 	elif characters.size() > 0:
 		var character = God.get_closest(characters, camera.get_position())
-		camera.target = character.get_position()
+		target = character.get_position()
+	target += camera.mouse_world_position() / get_viewport().size.normalized() * 0.2
+	target /= 1.2
+	var view_size = get_viewport().size * camera.zoom
+	var tx = clamp(target.x, - roomManager.get_width() / 2 + view_size.x / 2 - 16, + roomManager.get_width() / 2 - view_size.x / 2 + 16)
+	var ty = clamp(target.y, - roomManager.get_height() / 2 + view_size.y / 2 - 16, + roomManager.get_height() / 2 - view_size.y / 2 + 16)
+	camera.target = Vector2(tx, ty)
 	#var scale = sqrt(clamp(max_dis, 150, min(roomManager.get_width(), roomManager.get_height()))) / 50
 	#camera.zoom = Vector2(scale, scale)
 	
