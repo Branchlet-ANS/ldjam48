50cfc1d2c769029625f5b7d62e3b86b3226670ed
magnetenstad <tenstad.magne@gmail.com>
2021-04-25 17:37:10 +0200

    Separate Entity and Character

--- godot/world/reals/character/Character.gd
+++ godot/world/reals/character/Character.gd
@@ -1,26 +1,8 @@
-extends KinematicReal
+extends Entity
 
 class_name Character
 
-enum STATE {
-	idle,
-	target,
-	job,
-	attack
-}
-
 var inventory : Inventory = Inventory.new()
-var _state : int
-var velocity : Vector2 = Vector2(0, 0)
-var _target : Vector2 = Vector2(0, 0)
-var speed : float = 100
-var job : Real = null
-var job_timer : int = 0
-var interact_area : Area2D
-var attack_timer : int = 0
-var attack_target : KinematicReal
-var weapon : Weapon = null
-var _health : float = 100.0
 
 var player_step : AudioStreamPlayer2D
 var step_pos : Vector2 = Vector2.ZERO
@@ -32,16 +14,6 @@ func _init(id : String, name: String = "").(id, name):
 
 func _ready():
 	set_sprite("characters/character.png")
-	interact_area = Area2D.new()
-	var _collision_shape = CollisionShape2D.new()
-	var _shape = RectangleShape2D.new()
-	_shape.set_extents(Vector2(12, 12))
-	_collision_shape.set_shape(_shape)
-	collision_layer = 1 << 1
-	collision_mask = (1 << 0) + (1 << 2)
-	interact_area.add_child(_collision_shape)
-	interact_area.connect("body_entered", self, "_on_Area2D_body_entered")
-	add_child(interact_area)
 
 	player_step = AudioStreamPlayer2D.new()
 	add_child(player_step)
@@ -55,87 +27,8 @@ func _process(_delta):
 		step_pos = position
 		if(!player_step.playing):
 			player_step.play()
-	if get_state() == STATE.idle:
-		if is_instance_valid(attack_target):
-			set_state(STATE.attack)
-		if is_instance_valid(job):
-			set_target(job.transform.origin)
-	if get_state() == STATE.job:
-		perform_job()
-	if get_state() == STATE.attack:
-		attack_cycle(_delta)
-
-func _physics_process(delta):
-	if get_state() == STATE.target:
-		velocity = transform.origin.direction_to(_target) * speed
-		var velocity_prev = velocity
-		velocity = move_and_slide(velocity)
-		if transform.origin.distance_to(_target) < speed * delta * (1 if velocity == velocity_prev else 8):
-			if is_instance_valid(job) and job.transform.origin == get_target():
-				set_state(STATE.job)
-			else:
-				set_state(STATE.idle)
 
 func _on_Area2D_body_entered(body):
 	if body is Item:
 		inventory.add(body)
 		body.get_parent().remove_child(body)
-
-func set_job(interactable):
-	job = interactable
-
-func attack(enemy):
-	attack_target = enemy
-	set_state(STATE.attack)
-
-func get_state():
-	return _state
-
-func set_state(state):
-	_state = state
-	if state == STATE.idle:
-		_target = transform.origin
-	elif state == STATE.job:
-		job_timer = 100
-	elif state == STATE.attack:
-		attack_timer = 100
-
-func set_target(target):
-	_target = target
-	set_state(STATE.target)
-
-func perform_job():
-	job_timer -= 1
-	if job_timer == 0:
-		job.interact(self)
-		job = null
-		set_state(STATE.idle)
-
-func attack_cycle(delta):
-	if !is_instance_valid(attack_target):
-		set_state(STATE.idle)
-	attack_timer -= delta
-	if attack_timer <= 0:
-		strike(attack_target.position)
-		attack_timer = 100
-
-func strike(pos):
-	var p = weapon.get_projectile()
-	var projectile = Projectile.new("", "", p._speed, p._rotating,
-			p._dmg, p._inaccuracy, p._sprite_name, false, Vector2.ZERO, Vector2.ZERO)
-	get_parent().add_child(projectile)
-	projectile.fire((pos-position).normalized(), position)
-
-
-func get_target():
-	return _target
-
-func add_health(amount):
-	if (_health + amount) <= 100.0:
-		_health += amount
-	else:
-		_health = 100.0
-
-	if _health <= 0:
-		# delete fella
-		pass
