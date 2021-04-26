aa64b60f7eb438b28e7acf5962be147d27bbf58f
Erikseeeeen <43137654+Erikseeeeen@users.noreply.github.com>
2021-04-27 01:29:24 +0200

    music

--- godot/Music.gd
+++ godot/Music.gd
@@ -7,21 +7,20 @@ var tracks = [preload("res://assets/music/theme_0.wav"),
 	preload("res://assets/music/theme_1.wav"),
 	preload("res://assets/music/theme_2.wav"),
 	preload("res://assets/music/theme_3.wav")]
+var player : AudioStreamPlayer
 
 func _ready():
-	play_music(0)
+	player = AudioStreamPlayer.new()
+	add_child(player)
+	player.volume_db = -20
 	pass
 
 func _process(delta):
 	pass
 
 func play_music(corruption : int):
-	print("play")
-	var player : AudioStreamPlayer = AudioStreamPlayer.new()
 	var sfx = tracks[min(floor(corruption/3),3)]
 	sfx.set_stereo(true)
 	player.set_stream(sfx)
-	add_child(player)
 	player.play()
-	player.volume_db = -15
 
